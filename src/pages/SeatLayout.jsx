71d644d33e648af0263718c32d3be0514bae59df
coderrNishantt <nishntkumr627@gmail.com>
2025-07-06 19:53:54 +0530

    file strucrture

---
+++ src/pages/SeatLayout.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { dummyShowsData, dummyDateTimeData } from "../assets/assets";
+import Loading from "../components/Loading";
+import { ClockIcon } from "lucide-react";
+
+const SeatLayout = () => {
+  const { id, date } = useParams();
+  const [selectedSeat, setSelectedSeat] = useState([]);
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [showTime, setShowTime] = useState(null);
+  const [show, setShow] = useState(null);
+
+  const nav = useNavigate();
+
+  const getShow = async () => {
+    const show = dummyShowsData.find((show) => show._id === id);
+    if (show) {
+      setShow({
+        movie: show,
+        dateTime: dummyDateTimeData,
+      });
+    }
+  };
+
+  useEffect(() => {
+    getShow();
+  }, []);
+
+  return show ? (
+    <div
+      className={`flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 mt:pt-50`}
+    >
+      <div className="w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:sticky md:top-30 ">
+        <p className="text-lg font-semibold px-6">Availabe Timings</p>
+      </div>
+      <div className="mt-5 space y1">
+        {show.dateTime[date].map((item) => (
+          <div
+            key={item.time}
+            className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${
+              selectedTime?.time === item.time
+                ? "bg-primary text-white"
+                : "hover:bg-primary/20"
+            }`}
+          >
+            <ClockIcon className="w-4 h-4 " />
+            <p className="text-sm">{item.time}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+
+export default SeatLayout;
