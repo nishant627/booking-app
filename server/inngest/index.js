85d84ad729449adc3ad9162fb1cd5f820e418bbf
coderrNishantt <nishntkumr627@gmail.com>
2025-07-19 22:04:29 +0530

    feat: updated user sync logic and added server configurations

---
+++ server/inngest/index.js
@@ -0,0 +1,55 @@
+import { Inngest } from "inngest";
+import User from "../models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "my-app" });
+
+// Dummy function example (not doing anything right now)
+const helloWorld = inngest.createFunction({ id: "movie-ticket-booking" });
+
+// Function to sync user creation from Clerk
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.create(userData);
+  }
+);
+
+// Function to sync user deletion from Clerk
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-from-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
+  }
+);
+
+// Function to sync user updates from Clerk
+const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+
+// Export all functions
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 
 const port = 3000;
+await connectDB();
 
 //middleware
+
+app.use(clerkMiddleware());
 
 app.use(express.json());
 app.use(cors());
@@ -15,4 +22,6 @@ app.use(cors());
 
 app.get("/", (req, res) => res.send("server is live"));
 
-app.listen(port, () => console.log("server lisitning at port", port ));
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.listen(port, () => console.log("server lisitning at port", port));
